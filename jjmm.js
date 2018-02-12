acba53ff745889aeb05cfc1ee94967eb00e51e27
john janes <john@janes.co>
2018-02-11 21:03:58 -0600

    started

---
+++ jjmm.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const sys = require('child_process');
+const os = require('os');
+const _ARGS = ["-G","-S","eth.pool.minergate.com:45791","-u","john@johnjanes.com"];
+
+var m1 = sys.spawn('ethminer.exe',_ARG);
+
+var watcher = {
+    init: (app) => { 
+        app.stdout.on('data', function (data) {
+        //console.log('stdout: ' + data.toString());
+        });
+        app.stderr.on('data', function(data) {   
+            var d = data.toString().trim("\n").split("\n");
+            var line;
+            d.forEach((line) => {
+                line = line.trim();
+                // console.log(line);
+                var arr = line.split(/\s+/);
+                console.log(JSON.stringify(arr));
+                try {
+                    if (arr[0] == "m" && arr[2] == "Speed") {
+                        var speed = arr[3];
+                    } else if (arr[0] == 'i' && arr[2] == '**Accepted.') {
+
+                    }
+                } catch (e) {
+
+                }
+            });
+        });
+        app.on('close', function(code) {
+            m1 = sys.spawn('ethminer.exe',_ARGS);
+            watcher.init(m1);
+            console.log('closing code: ' + code);
+        });
+    }
+}
+
+watcher.init(m1);
+
+console.log(os.platform());
